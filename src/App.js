f7534768a114debc9f2d865fc4714975ef6c925c
inavis <rednaz.inavis@gmail.com>
2024-11-19 20:18:04 +0530

    app-routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { MainPage } from "./Pages/MainPage/MainPage";
+import { Error } from "./Pages/Error/Error";
+import { Rules } from "./Pages/Rules/Rules";
+
+// const MainPage = lazy(() => import('./Pages/MainPage/MainPage'));
+// const Error404 = lazy(() => import('./Pages/Error/Error'));
+// const Rules = lazy(()=> import('./Pages/Rules/Rules'))
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {/* exact match as / is common with url */}
+          <Route exact path='/' element={<MainPage />} />
+
+          <Route path='/terms-and-condition' element={<Rules />} />
+
+          {/* 404 error page when no url matches */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
