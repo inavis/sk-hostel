df8f57431a521d0d1f48485de5f43ee846031cd2
inavis <rednaz.inavis@gmail.com>
2024-11-25 21:12:35 +0530

    wrappin-with-contextProvider

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -2,14 +2,15 @@ import "./MainPage.css";
 import { Header } from "../../Components/Header/Header";
 import { Footer } from "../../Components/Footer/Footer";
 import Counter from "../../Components/Counter/Counter";
+import { StateProvider } from "../../Context";
 
 export function MainPage() {
   return (
-    <>
+    <StateProvider>
       <Header />
       <Counter />
       MainPage
       <Footer />
-    </>
+    </StateProvider>
   );
 }
