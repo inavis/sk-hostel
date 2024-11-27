db13a484a626c153003934fa55b5f33e858655c5
inavis <rednaz.inavis@gmail.com>
2024-11-27 10:50:39 +0530

    adding-banner-to-page

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -2,12 +2,14 @@ import "./MainPage.css";
 import { Header } from "../../Components/Header/Header";
 import { Footer } from "../../Components/Footer/Footer";
 import Counter from "../../Components/Counter/Counter";
+import { Banner } from "../../Components/Banner/Banner";
 import { StateProvider } from "../../Context";
 
 export function MainPage() {
   return (
     <StateProvider>
       <Header />
+      <Banner />
       <Counter />
       MainPage
       <Footer />
